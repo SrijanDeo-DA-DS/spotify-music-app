2039bbba41d51fd7206efe5b0c5c3f5ffadc6fa6
SrijanDeo-DA-DS <sd759@njit.edu>
2024-06-04 11:53:31 -0500

    initialize app structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This app will help all music enthusiasts to predict success/failure of a new music, get new song recommendations, insights module and analytics all in one place',
+    author='SrijanDeo',
+    license='',
+)
