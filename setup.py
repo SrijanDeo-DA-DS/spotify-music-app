27b6ad2c62e4f651fbd30678cbddce377f7d8512
SrijanDeo-DA-DS <sd759@njit.edu>
2024-06-04 12:40:54 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,20 @@
 from setuptools import find_packages, setup
+from typing import List
+
+REQUIREMENTS_FILE_NAME = 'requirements.txt'
+
+HYPHEN_E_DOT = "-e ."
+
+def get_requirements_list()->List[str]:
+    with open(REQUIREMENTS_FILE_NAME) as f:
+        requirement_list = f.readlines()
+        requirement_list = [i.replace("\n","") for i in requirement_list]
+
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
+
+        return requirement_list
+
 
 setup(
     name='src',
@@ -7,4 +23,5 @@ setup(
     description='This app will help all music enthusiasts to predict success/failure of a new music, get new song recommendations, insights module and analytics all in one place',
     author='SrijanDeo',
     license='',
+    install_requires = get_requirements_list()
 )
