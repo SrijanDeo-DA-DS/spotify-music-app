ddc590a8104c1cf86d3119de96fcc4294b0f09c6
SrijanDeo-DA-DS <sd759@njit.edu>
2024-06-12 11:31:03 -0500

    data ingestion update

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,7 @@ from dataclasses import dataclass
 from src.logger import logging
 from src.exception import CustomException
 from src.components.data_transformation import DataTransformation, DataTransformationConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -64,3 +65,5 @@ if __name__=='__main__':
     train_data_path, test_data_path, validation_data_path = obj.initiate_data_ingestion()
     data_transformation = DataTransformation()
     train_arr , test_arr, validation_arr,_ = data_transformation.initiate_data_transformation(train_data_path, test_data_path, validation_data_path)
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_training(train_arr, test_arr))
